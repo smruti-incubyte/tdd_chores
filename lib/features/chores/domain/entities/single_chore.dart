ea2a782a216ac2de3d2bd631a08ac780a20791b9
smruti <smruti@incubyte.co>
2026-02-25 21:25:23 +0530

    Green : enums, entity

---
+++ lib/features/chores/domain/entities/single_chore.dart
@@ -0,0 +1,18 @@
+import 'package:tdd_chores/core/enums/enums.dart';
+import 'package:equatable/equatable.dart';
+
+class SingleChoreEntity extends Equatable {
+  final String id;
+  final String name;
+  final DateTime dateTime;
+  final ChoreStatus status;
+
+  const SingleChoreEntity({
+    required this.id,
+    required this.name,
+    required this.dateTime,
+    required this.status,
+  });
+  @override
+  List<Object?> get props => [id, name, dateTime, status];
+}
--- test/chores/domain/chores_domain_test.dart
+++ test/chores/domain/chores_domain_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tdd_chores/core/enums/enums.dart';
+import 'package:tdd_chores/features/chores/domain/entities/single_chore.dart';
 
 void main() {
   test('should get list of single chores from repository', () {
