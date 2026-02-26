c2c22cf6ee90df244f1b6914857efe3d0d7c0a30
smruti <smruti@incubyte.co>
2026-02-26 09:23:48 +0530

    Red : models

--- test/chores/data/chore_repository_impl.dart
+++ test/chores/data/chore_repository_impl.dart
@@ -1,11 +1,30 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_chores/core/enums/enums.dart';
 import 'package:tdd_chores/features/chores/data/repositories/chore_repository_impl.dart';
+import 'package:tdd_chores/features/chores/domain/entities/single_chore.dart';
 
 void main() {
+  final tDateTime = DateTime(2024, 1, 1);
+
+  final tSingleChoreEntity = SingleChoreEntity(
+    id: '1',
+    name: 'Test Chore',
+    dateTime: tDateTime,
+    status: ChoreStatus.todo,
+  );
+
+  final tSingleChoreModel = SingleChoreModel(
+    id: '1',
+    name: 'Test Chore',
+    dateTime: tDateTime,
+    status: ChoreStatus.todo,
+  );
+
   test(
     'should return list of single chores entities from repository',
     () async {
       final repository = ChoreRepositoryImpl();
+      final result = await repository.getSingleChores();
     },
   );
 }
