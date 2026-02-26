dd203dcffe65d8a7fdd51d38ca55ef3a00c1a921
smruti <smruti@incubyte.co>
2026-02-26 09:18:08 +0530

    Red : repo impl

---
+++ test/chores/data/chore_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'should return list of single chores entities from repository',
+    () async {
+      final repository = ChoreRepositoryImpl();
+    },
+  );
+}
